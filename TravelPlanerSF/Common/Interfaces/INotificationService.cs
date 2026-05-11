b9b5bef14369add3abbb2299204db89474a48eb8
Dragan Milosevic <dragan@parity.io>
2026-05-11 10:44:38 +0200

    add INotificationService Remoting interface for inter-service communication

--- TravelPlanerSF/Common/Interfaces/INotificationService.cs
+++ TravelPlanerSF/Common/Interfaces/INotificationService.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Common.DTOs;
+using Common.Enums;
 
 namespace Common.Interfaces
 {
-    internal interface INotificationService
+    public interface INotificationService : IService
     {
+        // Share token operations (Reliable Dictionary)
+        Task<ShareLinkDTO> CreateShareTokenAsync(long travelPlanId, ShareAccessType accessType);
+        Task<ShareLinkDTO?> ValidateTokenAsync(string token);
+        Task<List<ShareLinkDTO>> GetSharesForPlanAsync(long travelPlanId);
+        Task<bool> RevokeShareAsync(long travelPlanId, long shareId);
+        Task<int> RevokeAllSharesForPlanAsync(long travelPlanId);
+
+        // Audit log operations (Reliable Queue)
+        Task PublishAuditEventAsync(AuditEventDTO auditEvent);
     }
 }
